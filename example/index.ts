1a9027102c908a9ec2cec37e7a36a6e82bb65d0f
Victor Cuena <victorcuena92@gmail.com>
2024-03-02 19:24:48 +0100

    Add documentation

--- example/index.ts
+++ example/index.ts
@@ -1,6 +1,12 @@
 import { BrevoClient } from "@cuecodes/emails";
 import type { EmailTemplates } from "@cuecodes/emails";
 
+/*
+ * Pro Tip:
+ * 
+ * Declare your template ids and their corresponding placeholders.
+ * This will give you a nice autocomplete when sending the emails ✨✨✨
+*/
 interface MyExampleEmailTemplates extends EmailTemplates {
     1: {
         name: string;
@@ -9,19 +15,24 @@ interface MyExampleEmailTemplates extends EmailTemplates {
         email: string;
     }
 }
-
 const client = new BrevoClient<MyExampleEmailTemplates>("YOUR_API_KEY");
 
-client.sendEmail({
-    templateId: 1,
-    params: {
-        // ❤️ We have autocomplete here ❤️
-        name: "John"
-    },
-    to: [{
-        email: "example@example.com",
-        name: "John Doe"
-    }]
-})
-    .then(() => console.log("Sent!"))
-    .catch((err) => console.error("Error :(", err));
+export const getClient = () => client;
+
+
+// Example:
+export const sendDemoEmail = () => {
+    client.sendEmail({
+        templateId: 1,
+        params: {
+            // ❤️ We have autocomplete here ❤️
+            name: "John"
+        },
+        to: [{
+            email: "example@example.com",
+            name: "John Doe"
+        }]
+    })
+        .then(() => console.log("Sent!"))
+        .catch((err) => console.error("Error :(", err));
+}
